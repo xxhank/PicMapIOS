0262b2ac50f88b407656b065230c471a8d62f3ed
wangchao <xxhank.boy@gmail.com>
2016-02-22 22:55:26 +0800

    显示关注和粉丝列表

---
+++ PicMapIOS/Scenes/Tourist/TouristFollowsCell.swift
@@ -0,0 +1,24 @@
+//
+//  TouristFollowsCell.swift
+//  PicMapIOS
+//
+//  Created by wangchaojs02 on 16/2/22.
+//  Copyright © 2016年 wangchaojs02. All rights reserved.
+//
+
+import UIKit
+
+class TouristFollowsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PicMapIOS/Scenes/Tourist/TouristFollowsController.swift
@@ -0,0 +1,12 @@
+//
+//  TouristFollowsController.swift
+//  PicMapIOS
+//
+//  Created by wangchaojs02 on 16/2/22.
+//  Copyright © 2016年 wangchaojs02. All rights reserved.
+//
+
+import UIKit
+
+class TouristFollowsController: PMIViewController {
+}
